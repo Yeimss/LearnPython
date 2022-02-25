0b83d1d0be58c9074ba2270fd8be4368adeb2601
206 <206@academicos.cesde.edu.co>
2022-02-25 18:58:13 -0500

    NTV001 Se crea el main.py con una introducción a python, desde creación de variables hasta condicionales

---
+++ HelloWorld/main.py
@@ -0,0 +1,29 @@
+#soy un comentario
+'''
+soy un comentario de bloque
+'''
+
+nombreUsuario="Chente Fernandez"
+
+#--------concatenar con f print----------
+print(f"Su nombre es: {nombreUsuario}")
+
+
+#-------tipos de variables---------------
+
+edadUsuario=23              #int/entero
+estatura=1.72               #double/decimal
+esHinchaDelMejor=True       #Booleano
+nombre="Juan"               #string
+
+
+n1=int(input("ingrese un numero: "))
+n2=int(input("ingrese un numero: "))
+suma=n1+n2
+print(f"la suma entre {n1} y {n2} es: {suma}")
+
+if n1<0:
+    print(f"{n1} es un numero negativo")
+else:
+    print(f"{n1} es un numero positivo")
+
