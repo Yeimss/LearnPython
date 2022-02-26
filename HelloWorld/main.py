58975316aa70e89e117cc1c74e8b32c4d004a800
206 <206@academicos.cesde.edu.co>
2022-02-25 20:46:32 -0500

    NTV001 se crea ejemplo de 3 posibles situaciones con una represa

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -1,34 +1,8 @@
-#soy un comentario
-'''
-soy un comentario de bloque
-'''
-
-nombreUsuario="Chente Fernandez"
-
-#--------concatenar con f print----------
-print(f"Su nombre es: {nombreUsuario}")
-
-
-#-------tipos de variables---------------
-
-edadUsuario=23              #int/entero
-estatura=1.72               #double/decimal
-esHinchaDelMejor=True       #Booleano
-nombre="Juan"               #string
-
-
-n1=int(input("ingrese un numero: "))
-n2=int(input("ingrese un numero: "))
-suma=n1+n2
-print(f"la suma entre {n1} y {n2} es: {suma}")
-
-if n1<0:
-    print(f"{n1} es un numero negativo")
-else:
-    print(f"{n1} es un numero positivo")
-
-
 nivelAgua=float(input("Digite la cantidad de agua de la represa: "))
-print(f"El nivel de agua es: {nivelAgua}")
 
-print("soy un cambio para agregarlo al Git")
+if nivelAgua < 200:
+    print("No tenemos aguita")
+elif nivelAgua < 450:
+    print("Todo bien, todo correcto")
+else:
+    print("Ojito que nos desbordamos")
